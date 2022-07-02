88d825095c6060e06f9fb79e303d532cdf097935
travis flake <tflake83@gmail.com>
2022-07-02 09:15:26 -0400

    1st commit

---
+++ index.js
@@ -0,0 +1,4 @@
+// create a user account
+//create a way for that custmer to to enter their customers info and add to mailing list
+// name, message, upload, link
+//firestore acct needs auto-generated id 
