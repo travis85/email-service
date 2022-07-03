69850c252ccb370c411b2da35e898156b6b82c8e
travis flake <tflake83@gmail.com>
2022-07-03 19:57:44 -0400

    emailform.js component

---
+++ email-service/src/EmailForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './App.css'
+import { Form, Button } from 'react-bootstrap';
+import AddNewUser from './AddNewUser.js'
+
+
+
+
+export default function EmailForm() {
+  return (
+    
+    <div className='emailDiv'>
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label> Email Header</Form.Label>
+                <Form.Control type="email"  defaultValue='User LLC' />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Upload</Form.Label>
+                <Form.Control type="file" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+
+        </Form>
+          
+        < AddNewUser />
+
+    </div>
+    
+  
+    
+  )
+}
+
