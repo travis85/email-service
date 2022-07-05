799871dc30c18abd769ed826d7225b9ad0430937
travis flake <tflake83@gmail.com>
2022-07-05 10:10:05 -0400

    connected to firestore

--- email-service/src/CustmerList.js
+++ email-service/src/CustmerList.js
@@ -1,20 +1,39 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from 'axios'
 import './App.css'
+import  {firestore} from './utils/firebase';
+import { collection, addDoc, onSnapshot, deleteDoc, doc, where, query, updateDoc, setDoc} from "firebase/firestore"; 
 
-const URL = 'https://jsonplaceholder.typicode.com/users'
+
 
 export default function App() {
-  const [employees, setEmployees] = React.useState([])
-  React.useEffect(() => {
-        getData()
-  }, [])
-  const getData = async () => {
+    const [employees, setEmployees] = React.useState([])
+    const [notesFromDb, setNotesFromDb] = useState([])
 
-        const response = await axios.get(URL)
-        setEmployees(response.data)
+    React.useEffect(() => {
+        getData()
+    }, [])
+    
+    const getData = async () => {
+        const collectionRef = collection(firestore,"email-service")
+        const queryParams = where('uid', '==', user.uid)
+        const notesQuery = query(collectionRef, queryParams)
+        // onsnapshot is used so the data will update itself
+        onSnapshot(notesQuery, (snapshot) =>{ 
+        setNotesFromDb(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id  })));
+        })
     }
+    const [user, setUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        uid: '',
+
+    })
+
+
 
     const removeData = (id) => {
 
@@ -25,7 +44,7 @@ export default function App() {
     }
 
     const renderHeader = () => {
-        let headerElement = ['id', 'name', 'email', 'phone', 'operation']
+        let headerElement = ['id', 'first name','Last Name', 'email', 'phone', 'operation']
 
         return headerElement.map((key, index) => {
             return <th key={index}>{key.toUpperCase()}</th>
@@ -33,13 +52,14 @@ export default function App() {
     }
 
     const renderBody = () => {
-        return employees && employees.map(({ id, name, email, phone }) => {
+        return notesFromDb.map(({ id, firstName, lastName, email, phoneNumber }) => {
             return (
                 <tr key={id}>
                     <td>{id}</td>
-                    <td>{name}</td>
+                    <td>{firstName}</td>
+                    <td>{lastName}</td>
                     <td>{email}</td>
-                    <td>{phone}</td>
+                    <td>{phoneNumber}</td>
                     <td className='opration'>
                         <button className='delete' onClick={() => removeData(id)}>Delete</button>
                         <button className='update' onClick={() => removeData(id)}>Update</button>
